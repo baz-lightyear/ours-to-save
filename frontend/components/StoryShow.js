14c6462a1af48b7c462386338162a389a3598d6e
Harry Kingdon <harry@harrykingdon.com>
2020-04-18 10:43:23 +0100

    it worked, did the same for story

--- frontend/components/StoryShow.js
+++ frontend/components/StoryShow.js
@@ -1,27 +1,16 @@
 import React, { Component } from 'react';
-import Head from 'next/head';
 import styled from 'styled-components';
-import { Query } from 'react-apollo';
 import Moment from 'react-moment';
 import {
     EmailShareButton,
     FacebookShareButton,
-    InstapaperShareButton,
-    LinkedinShareButton,
-    RedditShareButton,
     TwitterShareButton,
-    WhatsappShareButton,
 } from "react-share";
 import {
     EmailIcon,
     FacebookIcon,
-    InstapaperIcon,
-    LinkedinIcon,
-    RedditIcon,
     TwitterIcon,
-    WhatsappIcon,
 } from "react-share";
-import { SINGLE_STORY_QUERY } from './Apollo';
 import MapStoryShow from './MapStoryShow';
 import OtherArticles from './OtherArticles';
 
@@ -63,34 +52,25 @@ const Container = styled.div`
 class StoryShow extends Component {
     render() {
         return (
-            <>
-                <Head>
-                    <meta property="og:url"                content={`https://www.ourstosave.com/story?id=${this.props.story.id}/`} key="url"/>
-                    <meta property="og:title"              content={this.props.story.title} key="title"/>
-                    <meta property="og:description"        content={this.props.story.subtitle} key="description"/>
-                    <meta property="og:image"              content={this.props.story.image} key="image"/>
-                    <meta property="og:type"              content="article" key="type"/>
-                </Head>
-                <Container>
-                    <h1>{this.props.story.title}</h1>
-                    <small>Posted <Moment date={this.props.story.createdAt} format="Do MMM YYYY"/> by {this.props.story.author}</small>
-                    <br/>
-                    {this.props.story.image && <img id="image" src={this.props.story.image} alt={this.props.story.title} />}
-                    <p>{this.props.story.content}</p>
-                    <div className="sharing">
-                        <p>Share: </p>
-                        <div className="icons">
-                        <EmailShareButton url={window.location.href}><EmailIcon round={true}></EmailIcon></EmailShareButton>
-                        <FacebookShareButton url={window.location.href}><FacebookIcon round={true}></FacebookIcon></FacebookShareButton>
-                        <TwitterShareButton url={window.location.href}><TwitterIcon round={true}></TwitterIcon></TwitterShareButton>
-                        </div>
+            <Container>
+                <h1>{this.props.story.title}</h1>
+                <small>Posted <Moment date={this.props.story.createdAt} format="Do MMM YYYY"/> by {this.props.story.author}</small>
+                <br/>
+                {this.props.story.image && <img id="image" src={this.props.story.image} alt={this.props.story.title} />}
+                <p>{this.props.story.content}</p>
+                <div className="sharing">
+                    <p>Share: </p>
+                    <div className="icons">
+                    <EmailShareButton url={`https://www.ourstosave.com/story?id=${this.props.story.id}`}><EmailIcon round={true}></EmailIcon></EmailShareButton>
+                    <FacebookShareButton url={`https://www.ourstosave.com/story?id=${this.props.story.id}`}><FacebookIcon round={true}></FacebookIcon></FacebookShareButton>
+                    <TwitterShareButton url={`https://www.ourstosave.com/story?id=${this.props.story.id}`}><TwitterIcon round={true}></TwitterIcon></TwitterShareButton>
                     </div>
-                    <div className="mapContainer">
-                        <MapStoryShow story={this.props.story}/>
-                    </div>
-                    <OtherArticles story={this.props.story}/>
-                </Container>
-            </>
+                </div>
+                <div className="mapContainer">
+                    <MapStoryShow story={this.props.story}/>
+                </div>
+                <OtherArticles story={this.props.story}/>
+            </Container>
         );
     }
 }
--- frontend/pages/story.js
+++ frontend/pages/story.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
 import StoryShow from '../components/StoryShow';
-import { SINGLE_STORY_QUERY } from '../components/Apollo';
+import Head from 'next/head'
+import { endpoint, prodEndpoint } from '../config.js';
 
 class story extends Component {
+    static async getInitialProps(ctx) {
+
+        // get the feature id from the context
+
+        const id = ctx.query.id;
+        
+        // find the right url to query the yoga server, in production and development
+
+        const url = process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint
+
+        // query it 
+
+        const res = await fetch(`${url}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: `{ story(id: "${id}") { id title content longitude latitude morality createdAt author image }}` }),
+        })
+        
+        const payload = await res.json()
+        const story = payload.data.story
+
+        return { story };
+    }
     render() {
         return (
-            <Query
-                query={SINGLE_STORY_QUERY}
-                variables={{
-                    id: this.props.query.id,
-                }}
-            >
-            {({ error, loading, data }) => {
-                if (error) return <p>error</p>;
-                if (loading) return <p>Loading...</p>;
-                return (
-                    <StoryShow id={this.props.query.id} story={data.story}/>
-                )
-            }}
-            </Query>
+            <>
+                <Head>
+                    <meta property="og:url"                content={`https://www.ourstosave.com/story?id=${this.props.story.id}`} key='og:url'/>
+                    <meta property="og:title"              content={this.props.story.title} key='og:title'/>
+                    <meta property="og:description"        content={this.props.story.content} key='og:description'/>
+                    <meta property="og:image"              content={this.props.story.image} key='og:image'/>
+                    <meta property="og:type"               content="article" key='og:type'/>
+                </Head>
+                <StoryShow id={this.props.story.id} story={this.props.story}/>
+            </>  
         );
     }
 }
