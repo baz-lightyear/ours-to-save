25843022aa09cd9d33a09956a546ab9dda18f6b3
Harry Kingdon <harry@harrykingdon.com>
2020-06-19 13:37:35 +0100

    whatever

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
 export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `https://ours-to-save-yoga-prod.herokuapp.com/`;
+export const prodEndpoint = `http://www.yoga.ourstosave.com/`;
