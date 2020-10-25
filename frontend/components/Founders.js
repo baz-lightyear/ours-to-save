a08886377fbcb87c54b71906029a38894a093859
Harry Kingdon <harry@harrykingdon.com>
2020-10-25 15:49:41 +0000

    pushing to master and heroku

--- frontend/components/Founders.js
+++ frontend/components/Founders.js
@@ -16,7 +16,7 @@ class Founders extends Component {
         return (
             <Container>
                 <h3>We're grateful for the support of our Founding Members, including:</h3>
-                <p><em>Heather Phelps · Jacqui Hocking · Emelie Strand · Joanna Ka Wai Wong · Tash Seaton · Lee Murray · Johan Awan · David Keene · Katie Brown · Hugh Cutting · Katie Orr · Di Wildblood · Andrew Wildblood · Martin Spielverlag · Reiter Spielverlag · Neena Paul · Juliette Corrin · Iona Miller · Laura Cameron · Caitlin Mullin · Emma Corrin · Thomas Lingard · Esme Audland · Krisztina Bana · Finley Kamen · Edward Pinnegar · Gareth Phillips · Eleanor Hulm · Clara Meyer-Horn · Katie Ball · Jasper Vardag-Hunter · Thomas Prais · Holly Kingdon · Lou Edwards · Florence Li · Matt Kingdon · Alice Kingdon · George Horner</em></p>
+                <p><em>Heather Phelps · Jacqui Hocking · Emelie Strand · Joanna Ka Wai Wong · Tash Seaton · Lee Murray · Johan Awan · David Keene · Katie Brown · Hugh Cutting · Katie Orr · Di Wildblood · Andrew Wildblood · Martin Spielverlag · Reiter Spielverlag · Neena Paul · Juliette Corrin · Iona Miller · Laura Cameron · Caitlin Mullin · Emma Corrin · Thomas Lingard · Esme Audland · Krisztina Bana · Finley Kamen · Edward Pinnegar · Gareth Phillips · Eleanor Hulm · Clara Meyer-Horn · Katie Ball · Jasper Vardag-Hunter · Thomas Prais · Holly Kingdon · Lou Edwards · Florence Li · Matt Kingdon · Alice Kingdon · George Horner · and especially Abigail Anna Swan for design services</em></p>
             </Container>
         );
     }
--- frontend/pages/podcast.js
+++ frontend/pages/podcast.js
@@ -20,7 +20,7 @@ class podcast extends Component {
         return (
             <Container>
                 <h1>Listen to our podcast!</h1> 
-                <p>It's informative, engaging and a little different. We've made our first episode and we've got loads more coming. Massive thanks to <a href="https://twitter.com/bennaflinn?lang=en">Benedict Flinn</a>, our outstanding host, <strong>Jack Baz</strong> for help with sound design and <a href="https://abigailannaswan.myportfolio.com/" target="_blank">Abigail Anna Swan</a> for beautiful cover art.</p>
+                <p>It's informative, engaging and a little different. We've made our first episode and we've got loads more coming. Massive thanks to <a href="https://twitter.com/bennaflinn?lang=en">Benedict Flinn</a>, our outstanding host and <strong>Jack Baz</strong> for help with sound design.</p>
                 <p>If you'd like to be involved in some way, please email <a href="mailto: podcast@ourstosave.com">podcast@ourstosave.com</a>.</p>
                 <a href="https://open.spotify.com/episode/1hSJnKfUAUh20ahWN4Oiba" target="_blank"><button>Listen on Spotify now</button></a>
             </Container>
