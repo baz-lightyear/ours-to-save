73ec5f21ac53f342d4dc8cd7709e4c2ddda65d02
Harry Kingdon <harry@harrykingdon.com>
2021-01-13 13:50:45 +0000

    pushing to master and heroku

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,9 +2,8 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import React from 'react'
 import { GA_TRACKING_ID } from '../lib/gtag'
-import { hotjar } from 'react-hotjar';
 
-const hotjarId = "2194548"
+const hotjarSiteId = "2194548"
 const hotjarSnippetVersion = "6"
 
 export default class MyDocument extends Document {
@@ -37,8 +36,19 @@ export default class MyDocument extends Document {
           />
           {this.props.styleTags}
 
-          {/* This is Hotjar config - we have to edit the basic code to make it work with Next. See https://github.com/abdalla/react-hotjar/issues/26 */}
-          {(typeof window !== "undefined") && hotjar.initialize(hotjarId, hotjarSnippetVersion)}
+          {/* This is Hotjar config - we have to edit the basic code to make it work with Next. See https://stackoverflow.com/questions/60057718/dynamically-add-a-variable-to-an-external-js-script-with-nextjs */}
+
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:${hotjarSiteId},hjsv:${hotjarSnippetVersion}};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}} 
+          />
+
         </Head>
         <body>
           <Main />
